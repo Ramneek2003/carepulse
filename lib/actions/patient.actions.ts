917fabad54514032f943835566535a349178e879
Ramneek <ramneek2003kaur@gmail.com>
2024-08-25 23:45:38 +0530

    make appointment form page

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -48,6 +48,20 @@ export const getUser = async (userId: string) => {
   }
 };
 
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      NEXT_PUBLIC_DATABASE_ID!,
+      NEXT_PUBLIC_PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", userId)]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const registerPatient = async ({
   identificationDocument,
   ...patient
